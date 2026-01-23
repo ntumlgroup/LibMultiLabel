4942b805f749546aa62aa95d755818df4baf494f
chcwww <chcwww1@gmail.com>
2026-01-23 06:06:17 +0000

    update retrain examplef

--- run_exp.py
+++ run_exp.py
@@ -4,11 +4,6 @@ import grid
 import numpy as np
 from dataclasses import asdict
 
-import time
-import json
-from tqdm import tqdm
-import itertools
-
 
 def prune_model(*args, **kwargs):
     pass
@@ -19,7 +14,7 @@ if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.INFO)
-    np.random.seed(20250820)
+    np.random.seed(20260123)
 
     parser = argparse.ArgumentParser(description="Parse command-line arguments.")
     parser.add_argument("--dataset", type=str, default="EUR-Lex", help="Dataset name (e.g., AmazonCat-13K, EUR-Lex)")
@@ -28,9 +23,9 @@ if __name__ == "__main__":
 
     dataset = linear.load_dataset(args.data_format, f"data/{args.dataset}/train.{args.data_format}")  # , f"data/{dataset}/test.{args.data_format}"
 
+    retrain = True
     n_folds = 3
-    retrain = False
-    linear_technique = 'tree'
+    monitor_metrics = ["P@1", "P@3", "P@5"]
     search_space_dict = {
         'max_features': [10000, 20000],
         'K': [10, 100],
@@ -39,19 +34,21 @@ if __name__ == "__main__":
         'c': [0.1, 0.2],
     }
 
-    # for i in search_space:
-    #     print(i)
-
-    search = grid.GridSearch(dataset, n_folds)
-    scores = search(search_space_dict)
-    print(scores)
-    breakpoint()
+    search = grid.GridSearch(dataset, n_folds, monitor_metrics)
+    cv_scores = search(search_space_dict)
+    sorted_cv_scores = sorted(cv_scores.items(), key=lambda x: x[1][monitor_metrics[0]], reverse=True)
+    print(sorted_cv_scores)
 
     if retrain:
-        # TODO
-        best_params = None
-        model = linear.LINEAR_TECHNIQUES[linear_technique](
-                    dataset["train"]["y"],
-                    dataset["train"]["x"],
-                    **asdict(best_params.linear),
+        # TODO: test set
+        best_params, best_cv_scores = list(sorted_cv_scores)[0]
+        print(best_params, best_cv_scores)
+
+        preprocessor = linear.Preprocessor(tfidf_params=asdict(best_params.tfidf))
+        transformed_dataset = preprocessor.fit_transform(dataset)
+        model = linear.train_tree(
+                    transformed_dataset["train"]["y"],
+                    transformed_dataset["train"]["x"],
+                    best_params.linear_options,
+                    **asdict(best_params.tree),
                 )
